87ca97a01e7a56153a7bc793ecc0706d427cc8de
2Kelvin <kvin.mwangi254@gmail.com>
2022-10-30 22:25:30 +0300

    filtered OtherScientists

--- app.js
+++ app.js
@@ -1,136 +1,166 @@
 var people = [{
-    id: 0,
-    name: "Creola Katherine Johnson",
-    profession: "Mathematician",
-    accomplishment: "Spaceflight calculations",
-    image_src: "../images/kate.jpg"
+  id: 0,
+  name: "Creola Katherine Johnson",
+  profession: "Mathematician",
+  accomplishment: "Spaceflight calculations",
+  image_src: "../images/kate.jpg"
 }, {
-    id: 1,
-    name: "Mario Jose Molina-Pasquel Henriquez",
-    profession: "Chemist",
-    accomplishment: "discovery of Arctic ozone hole",
-    image_src: "../images/mario.jpg"
+  id: 1,
+  name: "Mario Jose Molina-Pasquel Henriquez",
+  profession: "Chemist",
+  accomplishment: "discovery of Arctic ozone hole",
+  image_src: "../images/mario.jpg"
 }, {
-    id: 2,
-    name: "Mohammed Abdus Salam",
-    profession: "Physicist",
-    accomplishment: "electromagnetism theory",
-    image_src: "../images/abdus.jpg"
+  id: 2,
+  name: "Mohammed Abdus Salam",
+  profession: "Physicist",
+  accomplishment: "electromagnetism theory",
+  image_src: "../images/abdus.jpg"
 }, {
-    id: 3,
-    name: "Percy Lavon Julian",
-    profession: "Chemist",
-    accomplishment: "pioneering cortisone drugs, steroids and birth control pills",
-    image_src: "../images/lavon.jpg"
+  id: 3,
+  name: "Percy Lavon Julian",
+  profession: "Chemist",
+  accomplishment: "pioneering cortisone drugs, steroids and birth control pills",
+  image_src: "../images/lavon.jpg"
 }, {
-    id: 4,
-    name: "Subrhmanyan Chandrasekhar",
-    profession: "Astrophysicist",
-    accomplishment: "white dwarf star mass calculations",
-    image_src: "../images/chandra.jpg"
+  id: 4,
+  name: "Subrhmanyan Chandrasekhar",
+  profession: "Astrophysicist",
+  accomplishment: "white dwarf star mass calculations",
+  image_src: "../images/chandra.jpg"
 }];
 
 // -----> filter() <-------
 function ChemistPeople() {
-    // returns the whole object of the person whose profession is a chemist
-    var chemists = people.filter(function (person) {
-        return person.profession === "Chemist";
-    });
+  // returns the whole object of the person whose profession is a chemist
+  var chemists = people.filter(function (person) {
+    return person.profession === "Chemist";
+  });
 
-    // mapping over the chemists array to display each person's name from the object items
-    var listChemists = chemists.map(function (person) {
-        return React.createElement(
-            "li",
-            { key: person.id },
-            React.createElement("img", {
-                src: person.image_src,
-                alt: person.name
-            }),
-            React.createElement(
-                "p",
-                null,
-                React.createElement(
-                    "b",
-                    null,
-                    person.name,
-                    ":"
-                ),
-                " " + person.profession + " known for " + person.accomplishment
-            )
-        );
-    });
-
+  // mapping over the chemists array to display each person's name from the object items
+  var listChemists = chemists.map(function (person) {
     return React.createElement(
-        "ul",
+      "li",
+      { key: person.id },
+      React.createElement("img", { src: person.image_src, alt: person.name }),
+      React.createElement(
+        "p",
         null,
-        listChemists
+        React.createElement(
+          "b",
+          null,
+          person.name,
+          ":"
+        ),
+        " " + person.profession + " known for " + person.accomplishment
+      )
     );
+  });
+
+  return React.createElement(
+    "ul",
+    null,
+    listChemists
+  );
+}
+
+function NotChemists() {
+  var otherScientists = people.filter(function (person) {
+    return person.profession !== "Chemist";
+  });
+  var otherList = otherScientists.map(function (person) {
+    return React.createElement(
+      "li",
+      { key: person.name },
+      React.createElement("img", { src: person.image_src, alt: person.name }),
+      React.createElement(
+        "p",
+        null,
+        React.createElement(
+          "b",
+          null,
+          person.name,
+          ":"
+        ),
+        " " + person.profession + " known for " + person.accomplishment
+      )
+    );
+  });
+
+  return React.createElement(
+    "ul",
+    null,
+    otherList
+  );
 }
 
 // -----> map() <-------
 function AllScientists() {
-    var listOfPeople = people.map(function (person) {
-        return React.createElement(
-            "li",
-            { key: person.id },
-            React.createElement("img", {
-                src: person.image_src,
-                alt: person.name
-            }),
-            React.createElement(
-                "p",
-                null,
-                React.createElement(
-                    "b",
-                    null,
-                    person.name,
-                    ":"
-                ),
-                " " + person.profession + " known for " + person.accomplishment
-            )
-        );
-    });
-
+  var listOfPeople = people.map(function (person) {
     return React.createElement(
-        "ul",
+      "li",
+      { key: person.id },
+      React.createElement("img", { src: person.image_src, alt: person.name }),
+      React.createElement(
+        "p",
         null,
-        listOfPeople
+        React.createElement(
+          "b",
+          null,
+          person.name,
+          ":"
+        ),
+        " " + person.profession + " known for " + person.accomplishment
+      )
     );
+  });
+
+  return React.createElement(
+    "ul",
+    null,
+    listOfPeople
+  );
 }
 
 export default function App() {
-    return React.createElement(
-        "div",
-        { className: "app" },
-        React.createElement(
-            "h1",
-            null,
-            "Filtering and Mapping a Scientists List"
-        ),
-        React.createElement(
-            "h2",
-            null,
-            "All Scientists"
-        ),
-        React.createElement(AllScientists, null),
-        React.createElement(
-            "h2",
-            null,
-            "Chemist Scientists"
-        ),
-        React.createElement(ChemistPeople, null)
-    );
+  return React.createElement(
+    "div",
+    { className: "app" },
+    React.createElement(
+      "h1",
+      null,
+      "Filtering and Mapping a Scientists List"
+    ),
+    React.createElement(
+      "h2",
+      null,
+      "Chemist Scientists"
+    ),
+    React.createElement(ChemistPeople, null),
+    React.createElement(
+      "h2",
+      null,
+      "Other Scientists"
+    ),
+    React.createElement(NotChemists, null),
+    React.createElement(
+      "h2",
+      null,
+      "All Scientists"
+    ),
+    React.createElement(AllScientists, null)
+  );
 }
 // react
-var rootNode = document.getElementById('react_root_node');
+var rootNode = document.getElementById("react_root_node");
 var root = ReactDOM.createRoot(rootNode);
 root.render(React.createElement(App, null));
 
 // NOTES
-// You will often need to show several instances of the same component 
+// You will often need to show several instances of the same component
 // ...using different data when building interfaces
 // E.G. lists of comments & galleries of profile images
-// you can move/store that data in JavaScript objects and arrays 
+// you can move/store that data in JavaScript objects and arrays
 // ...and use methods like map() and filter() to render lists of components from them
 
 // 4 steps to creating <li> components from an array: (MAPPING COMPONENTS)
@@ -155,7 +185,7 @@ root.render(React.createElement(App, null));
 // the arraow function automatically returns the expresion after the '=>' symbol in a single line of code
 // ...so you don't have to manually write return
 // however, if you use '{}' '=>', you MUST include the return keyword
-//  {} means you'll be using more than one line of code 
+//  {} means you'll be using more than one line of code
 
 // You need to give each array item a key
 // ...a string or a number that uniquely identifies it among other items in the array
@@ -166,7 +196,7 @@ root.render(React.createElement(App, null));
 // hence helping it make the correct updates to the DOM tree
 // ***always include keys in your data (array) rather than creating them on the go
 
-// when you need your mapped list item rendered in several DOM nodes, just wrap it all up in a single <div> or 
+// when you need your mapped list item rendered in several DOM nodes, just wrap it all up in a single <div> or
 // ...use <since Fragment> since <></> doesn't take in attributes like the much needed key in our case
 // fragments disappear from the DOM so it will just render the components inside
 
@@ -175,7 +205,7 @@ root.render(React.createElement(App, null));
 //=> (However, it’s okay to use the same key types for JSX nodes in DIFFERENT arrays)
 // Keys must not change! Don’t generate them while rendering.
 // keys let us uniquely identify & tell apart an item among its siblings
-// /the index the item has in the array may change making it harder to identify it over time 
+// /the index the item has in the array may change making it harder to identify it over time
 // ...but keys are tagged on the item for life
 // generating keys on the fly with e.g Math.random() is bad because the key will never match with itself every time there's a render
 // just manually create a key ID in the item's data, that you can use
--- src/app.js
+++ src/app.js
@@ -1,106 +1,123 @@
 const people = [
-    {
-        id: 0,
-        name: "Creola Katherine Johnson",
-        profession: "Mathematician",
-        accomplishment: "Spaceflight calculations",
-        image_src: "../images/kate.jpg"
-    },
-    {
-        id: 1,
-        name: "Mario Jose Molina-Pasquel Henriquez",
-        profession: "Chemist",
-        accomplishment: "discovery of Arctic ozone hole",
-        image_src: "../images/mario.jpg"
-    },
-    {
-        id: 2,
-        name: "Mohammed Abdus Salam",
-        profession: "Physicist",
-        accomplishment: "electromagnetism theory",
-        image_src: "../images/abdus.jpg"
-    },
-    {
-        id: 3,
-        name: "Percy Lavon Julian",
-        profession: "Chemist",
-        accomplishment: "pioneering cortisone drugs, steroids and birth control pills",
-        image_src: "../images/lavon.jpg"
-    },
-    {
-        id: 4,
-        name: "Subrhmanyan Chandrasekhar",
-        profession: "Astrophysicist",
-        accomplishment: "white dwarf star mass calculations",
-        image_src: "../images/chandra.jpg"
-    }
+  {
+    id: 0,
+    name: "Creola Katherine Johnson",
+    profession: "Mathematician",
+    accomplishment: "Spaceflight calculations",
+    image_src: "../images/kate.jpg",
+  },
+  {
+    id: 1,
+    name: "Mario Jose Molina-Pasquel Henriquez",
+    profession: "Chemist",
+    accomplishment: "discovery of Arctic ozone hole",
+    image_src: "../images/mario.jpg",
+  },
+  {
+    id: 2,
+    name: "Mohammed Abdus Salam",
+    profession: "Physicist",
+    accomplishment: "electromagnetism theory",
+    image_src: "../images/abdus.jpg",
+  },
+  {
+    id: 3,
+    name: "Percy Lavon Julian",
+    profession: "Chemist",
+    accomplishment:
+      "pioneering cortisone drugs, steroids and birth control pills",
+    image_src: "../images/lavon.jpg",
+  },
+  {
+    id: 4,
+    name: "Subrhmanyan Chandrasekhar",
+    profession: "Astrophysicist",
+    accomplishment: "white dwarf star mass calculations",
+    image_src: "../images/chandra.jpg",
+  },
 ];
 
 // -----> filter() <-------
 function ChemistPeople() {
-    // returns the whole object of the person whose profession is a chemist
-    const chemists = people.filter((person) => person.profession === "Chemist");
+  // returns the whole object of the person whose profession is a chemist
+  const chemists = people.filter((person) => person.profession === "Chemist");
 
-    // mapping over the chemists array to display each person's name from the object items
-    const listChemists = chemists.map(person =>
-        <li key={person.id}>
-            <img
-                src={person.image_src}
-                alt={person.name}
-            />
-            <p>
-                <b>{person.name}:</b>{` ${person.profession} known for ${person.accomplishment}`}
-            </p>
-        </li>
-    );
+  // mapping over the chemists array to display each person's name from the object items
+  const listChemists = chemists.map((person) => (
+    <li key={person.id}>
+      <img src={person.image_src} alt={person.name} />
+      <p>
+        <b>{person.name}:</b>
+        {` ${person.profession} known for ${person.accomplishment}`}
+      </p>
+    </li>
+  ));
 
-    return <ul>{listChemists}</ul>
+  return <ul>{listChemists}</ul>;
+}
+
+function NotChemists() {
+  const otherScientists = people.filter(
+    (person) => person.profession !== "Chemist"
+  );
+  const otherList = otherScientists.map((person) => (
+    <li key={person.name}>
+      <img src={person.image_src} alt={person.name} />
+      <p>
+        <b>{person.name}:</b>
+        {` ${person.profession} known for ${person.accomplishment}`}
+      </p>
+    </li>
+  ));
+
+  return <ul>{otherList}</ul>;
 }
 
 // -----> map() <-------
 function AllScientists() {
-    const listOfPeople = people.map((person) =>
-        <li key={person.id}>
-            <img
-                src={person.image_src}
-                alt={person.name}
-            />
-            <p>
-                <b>{person.name}:</b>{` ${person.profession} known for ${person.accomplishment}`}
-            </p>
-        </li>);
+  const listOfPeople = people.map((person) => (
+    <li key={person.id}>
+      <img src={person.image_src} alt={person.name} />
+      <p>
+        <b>{person.name}:</b>
+        {` ${person.profession} known for ${person.accomplishment}`}
+      </p>
+    </li>
+  ));
 
-    return <ul>{listOfPeople}</ul>;
+  return <ul>{listOfPeople}</ul>;
 }
 
 export default function App() {
-    return (
-        <div className="app">
-            <h1>Filtering and Mapping a Scientists List</h1>
-            <h2>All Scientists</h2>
-            <AllScientists />
-            <h2>Chemist Scientists</h2>
-            <ChemistPeople />
-        </div>
-    );
+  return (
+    <div className="app">
+      <h1>Filtering and Mapping a Scientists List</h1>
+      <h2>Chemist Scientists</h2>
+      <ChemistPeople />
+      <h2>Other Scientists</h2>
+      <NotChemists />
+      <h2>All Scientists</h2>
+      <AllScientists />
+    </div>
+  );
 }
 // react
-const rootNode = document.getElementById('react_root_node');
+const rootNode = document.getElementById("react_root_node");
 const root = ReactDOM.createRoot(rootNode);
 root.render(<App />);
 
 // NOTES
-// You will often need to show several instances of the same component 
+// You will often need to show several instances of the same component
 // ...using different data when building interfaces
 // E.G. lists of comments & galleries of profile images
-// you can move/store that data in JavaScript objects and arrays 
+// you can move/store that data in JavaScript objects and arrays
 // ...and use methods like map() and filter() to render lists of components from them
 
 // 4 steps to creating <li> components from an array: (MAPPING COMPONENTS)
-    // move the data into an array & give each array item an 'id' to use as the list item key
-    // create a map array variable & map the original array to create a <li> item of each item in the array
-    // give each <li> item the key you assigned them before
-    // return your mapped items variable containing all mapped list items & place it in a <ul></ul>
+// move the data into an array & give each array item an 'id' to use as the list item key
+// create a map array variable & map the original array to create a <li> item of each item in the array
+// give each <li> item the key you assigned them before
+// return your mapped items variable containing all mapped list items & place it in a <ul></ul>
 
 // ** map() creates a new array; it doesn't modify the original array
 // to filter out/ serach out data from an array of items, use filter() method
@@ -110,15 +127,15 @@ root.render(<App />);
 // ...i.e the items you actually searched for
 
 // FILTERING COMPONENTS steps
-    // create a filter array variable
-    // filter the original array with the condition you want; the array items that pass the test/ condition will be returned in the filter array
-    // create a new map array: map the filtered array to return <li> items of the filtered array
-    // return this mapped array variable in a <ul></ul> to diaplay the list
+// create a filter array variable
+// filter the original array with the condition you want; the array items that pass the test/ condition will be returned in the filter array
+// create a new map array: map the filtered array to return <li> items of the filtered array
+// return this mapped array variable in a <ul></ul> to diaplay the list
 
 // the arraow function automatically returns the expresion after the '=>' symbol in a single line of code
 // ...so you don't have to manually write return
 // however, if you use '{}' '=>', you MUST include the return keyword
-//  {} means you'll be using more than one line of code 
+//  {} means you'll be using more than one line of code
 
 // You need to give each array item a key
 // ...a string or a number that uniquely identifies it among other items in the array
@@ -129,16 +146,16 @@ root.render(<App />);
 // hence helping it make the correct updates to the DOM tree
 // ***always include keys in your data (array) rather than creating them on the go
 
-// when you need your mapped list item rendered in several DOM nodes, just wrap it all up in a single <div> or 
+// when you need your mapped list item rendered in several DOM nodes, just wrap it all up in a single <div> or
 // ...use <since Fragment> since <></> doesn't take in attributes like the much needed key in our case
 // fragments disappear from the DOM so it will just render the components inside
 
 // Rules of keys
-    // Keys must be unique among siblings.
-        //=> (However, it’s okay to use the same key types for JSX nodes in DIFFERENT arrays)
-    // Keys must not change! Don’t generate them while rendering.
+// Keys must be unique among siblings.
+//=> (However, it’s okay to use the same key types for JSX nodes in DIFFERENT arrays)
+// Keys must not change! Don’t generate them while rendering.
 // keys let us uniquely identify & tell apart an item among its siblings
-// /the index the item has in the array may change making it harder to identify it over time 
+// /the index the item has in the array may change making it harder to identify it over time
 // ...but keys are tagged on the item for life
 // generating keys on the fly with e.g Math.random() is bad because the key will never match with itself every time there's a render
 // just manually create a key ID in the item's data, that you can use
